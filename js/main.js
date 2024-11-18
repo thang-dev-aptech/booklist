e63584b0f0478fe6f7ffd0074d369f9b213df2a4
METACOM <metacom@MacBook-Pro-cua-Thang.local>
2024-11-18 19:59:10 +0700

    booklist

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ function sign(){
     let a = document.getElementById('s1').value;
     let b = document.getElementById('s2').value;
     if (a == 'admin' && b == '123' ){
-        window.location.href = "bai5.html";
+        window.location.href = "bai6.html";
     }
     else{
-        alert('Đăng nhập thất bại !!!');
+        alert('login false !!!');
     }
 }
--- js/main6.js
+++ js/main6.js
@@ -4,29 +4,35 @@ const addBook = () => {
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
     let price = document.getElementById('price').value;
-    const addBook = document.createElement('div');
-    let obj = {id, title, author, price};
-    addBook.className = 'book';
-    addBook.style.marginTop='10px';
-    addBook.style.textAlign = 'left';
-    addBook.style.marginBottom = '10px';
-    addBook.style.padding = '7px';
-    addBook.style.borderBottom = '1px solid #ccc';    
-    addBook.style.fontSize = '17px';
-    addBook.style.fontWeight= 'normal';
-    addBook.innerHTML = 
-    `ID: <strong>${id}</strong> - Title: ${title} - Author: ${author}, Price: ${price}`
-    const booklist = document.getElementById('bookList');
-    booklist.appendChild(addBook);
-    ds.push({id, title, author, price});
-    for(let i = 0; i < Object.keys(obj).length; i++){
-        if(obj[0] == id){
+    let checkId = false;
+    for(let i = 0; i < ds.length; i++){
+        if(ds[i].id === id){ 
+            checkId = true;
             alert('ID này đã tồn tại');
-            return;
+            break;
         }
+    }
+    if(!checkId){
+        const addBook = document.createElement('div');
+        let obj = {id, title, author, price};
+        addBook.className = 'book';
+        addBook.style.marginTop='10px';
+        addBook.style.textAlign = 'left';
+        addBook.style.marginBottom = '10px';
+        addBook.style.padding = '7px';
+        addBook.style.borderBottom = '1px solid #ccc';    
+        addBook.style.fontSize = '17px';
+        addBook.style.fontWeight= 'normal';
+        addBook.innerHTML = 
+        `ID: <strong>${id}</strong> - Title: ${title} - Author: ${author}, Price: ${price}`
+        const book = document.getElementById('bookList');
+        book.appendChild(addBook);
+        
+        ds.push({id, title, author, price});
     }
     document.getElementById('id').value = "";
     document.getElementById('title').value = "";
     document.getElementById('author').value = "";
     document.getElementById('price').value = "";
+    
 }
