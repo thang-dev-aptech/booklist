e69f7ee24d70a9a23c98775e402c9bd1ce9b8cf2
METACOM <metacom@MacBook-Pro-cua-Thang.local>
2024-11-22 09:58:47 +0700

    editdelete

--- js/main6.js
+++ js/main6.js
@@ -1,39 +1,59 @@
 let ds = [];
-const addBook = () => {
-    let id  = document.getElementById('id').value;
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let price = document.getElementById('price').value;
-    let checkId = false;
-    for(let i = 0; i < ds.length; i++){
-        if(ds[i].id === id){ 
-            checkId = true;
-            alert('ID này đã tồn tại');
-            break;
-        }
-    }
-    if(!checkId){
-        const book = document.getElementById('bookList');
-        const addBook = document.createElement('div');
-        let obj = {id, title, author, price};
-        addBook.className = 'book';
-        
-        addBook.innerHTML = 
-        `ID: <strong>${id}</strong> - Title: ${title} - Author: ${author}, Price: ${price}`;
-        const actionButton = document.createElement('div');
-        actionButton.className = 'action';
-        const editBook = document.createElement('button');
-        editBook.className = 'editBook';
-        editBook.innerHTML = 'Edit';
-        
-        actionButton.appendChild(addBook);
-        actionButton.appendChild(editBook);
-        book.appendChild(actionButton);
-        ds.push({id, title, author, price});
-    }
-    document.getElementById('id').value = "";
-    document.getElementById('title').value ="";
-    document.getElementById('author').value ="";
-    document.getElementById('price').value ="";
+
+function addBook(){
+    const id = document.getElementById("id").value;
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const price = document.getElementById("price").value;
+    const obj = {id, title, author, price};
+    ds.push(obj);
+    displayBooks();
+    clearForm();
     
+    
+}
+
+function displayBooks(){
+    const list = document.getElementById("bookList");
+    list.innerHTML = `<h3>Book List</h3>`;
+    ds.forEach((book) => {
+        const newBook = document.createElement("div");
+        newBook.className = "newBook";
+        newBook.innerHTML = 
+        `id: <strong>${book.id}</strong> - title: ${book.title} - author: ${book.author} - price: ${book.price}`;
+        
+        const container = document.createElement("div");
+        container.className = "actionContainer";
+        list.appendChild(container);
+        const editButton = document.createElement("button");
+        editButton.className = "edit"
+        editButton.textContent = "Edit";    
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "delete";
+        deleteButton.textContent = "Delete";
+        deleteButton.onclick = () =>{
+            const index = ds.findIndex((b) => b.id === book.id);
+            if(index!== -1){
+                ds.splice(index, 1);
+            }
+            displayBooks();
+            clearForm();
+        }
+        container.appendChild(newBook);
+        container.appendChild(editButton);
+        container.appendChild(deleteButton);
+
+        list.appendChild(container);
+        
+
+
+        
+    });
+}
+function clearForm(){
+    document.getElementById("id").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("price").value = "";
+    document.getElementById("error").textContent = "";
 }
